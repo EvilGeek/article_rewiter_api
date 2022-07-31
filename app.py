aaddf938a6d1f1824f9e58e9067404e415547f0f
vaibhavchandra2007 <110343574+vaibhavchandra2007@users.noreply.github.com>
2022-07-31 20:29:09 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,60 @@
+from Rewriter import Rewriter
+
+import json
+
+import os
+
+from flask import Flask,render_template,url_for,request,flash,jsonify
+
+templates = os.path.join(os.getcwd(),"templates/")
+
+app = Flask(__name__, template_folder=templates)
+
+app.config['SECRET_KEY'] = "secrestsArePrivate
+
+
+
+@app.route("/",methods=['POST','GET'])
+
+def index():
+
+    text = request.form.get("text")
+
+    print(text)
+
+    if text != None:
+
+        rewriter = Rewriter(text)
+
+        rewritten = rewriter.main()[0]
+
+        return render_template("index.html",retext=rewritten,originaltxt=text)
+
+    return render_template("index.html")
+  
+
+@app.route("/v1/api")
+
+def api():
+
+    text = request.args.get("text", default = "*", type = str)
+
+    if text != "*":
+
+        rewriter = Rewriter(text)
+
+        rewritten = rewriter.main()[0]
+
+        return jsonify(status="Successful", text=rewritten, author="Vaibhav Chandra")
+
+    else:
+
+        return jsonify(status="Unsuccessful", text=None, author="Vaibhav Chandra")
+
+
+if __name__ == "__main__":
+
+    # debug = true will enable the debugger
+
+    app.run(debug=True)
+
